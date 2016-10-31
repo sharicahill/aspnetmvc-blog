6efebc879c81c3ae9abb3c2da56c83432306e5e0
Shari Cahill <sharicahill@gmail.com>
2016-10-31 13:55:47 +1000

    Implement password hash and check using BCrypt

--- SimpleBlog/Models/User.cs
+++ SimpleBlog/Models/User.cs
@@ -5,6 +5,13 @@ namespace SimpleBlog.Models
 {
     public class User
     {
+        private const int WorkFactor = 13;
+
+        public static void FakeHash()
+        {
+            BCrypt.Net.BCrypt.HashPassword("", WorkFactor);
+        }
+
         public virtual int Id { get; set; }
         public virtual string Username { get; set; }
         public virtual string Email { get; set; }
@@ -12,7 +19,12 @@ namespace SimpleBlog.Models
 
         public virtual void SetPassword(string password)
         {
-            PasswordHash = "IGNORE ME";
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public virtual bool CheckPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
         }
     }
 
