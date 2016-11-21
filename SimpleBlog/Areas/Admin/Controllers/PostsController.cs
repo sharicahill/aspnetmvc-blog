42bc7f24151a25766aebd0a87a62344023d8a72e
Shari Cahill <sharicahill@gmail.com>
2016-11-21 12:16:26 +1000

    Added our tag editor on our post form

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 ﻿using NHibernate.Linq;
 using SimpleBlog.Areas.Admin.ViewModels;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Infrastructure.Extensions;
 using SimpleBlog.Models;
 using System;
 using System.Collections.Generic;
@@ -36,7 +37,13 @@ namespace SimpleBlog.Areas.Admin.Controllers
         {
             return View("Form", new PostsForm
             {
-                IsNew = true
+                IsNew = true,
+                Tags = Database.Session.Query<Tag>().Select(tag => new TagCheckbox
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    IsChecked = false
+                }).ToList()
             });
         }
 
@@ -52,7 +59,13 @@ namespace SimpleBlog.Areas.Admin.Controllers
                 PostId = id,
                 Content = post.Content,
                 Slug = post.Slug,
-                Title = post.Title
+                Title = post.Title,
+                Tags = Database.Session.Query<Tag>().Select(tag => new TagCheckbox
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    IsChecked = post.Tags.Contains(tag)
+                }).ToList()
             });
         }
 
@@ -64,6 +77,8 @@ namespace SimpleBlog.Areas.Admin.Controllers
             if (!ModelState.IsValid)
                 return View(form);
 
+            var selectedTags = ReconcileTags(form.Tags).ToList();
+
             Post post;
             if (form.IsNew)
             {
@@ -72,6 +87,9 @@ namespace SimpleBlog.Areas.Admin.Controllers
                     CreatedAt = DateTime.UtcNow,
                     User = Auth.User,
                 };
+
+                foreach (var tag in selectedTags)
+                    post.Tags.Add(tag);
             }
             else
             {
@@ -81,6 +99,12 @@ namespace SimpleBlog.Areas.Admin.Controllers
                     return HttpNotFound();
 
                 post.UpdatedAt = DateTime.UtcNow;
+
+                foreach (var toAdd in selectedTags.Where(t => !post.Tags.Contains(t)))
+                    post.Tags.Add(toAdd);
+
+                foreach (var toRemove in post.Tags.Where(t => !selectedTags.Contains(t)).ToList())
+                    post.Tags.Remove(toRemove);
             }
 
             post.Title = form.Title;
@@ -125,6 +149,34 @@ namespace SimpleBlog.Areas.Admin.Controllers
             post.DeletedAt = null;
             Database.Session.Update(post);
             return RedirectToAction("Index");
+        }
+
+        private IEnumerable<Tag> ReconcileTags(IEnumerable<TagCheckbox> tags)
+        {
+            foreach (var tag in tags.Where(t => t.IsChecked))
+            {
+                if (tag.Id != null)
+                {
+                    yield return Database.Session.Load<Tag>(tag.Id);
+                    continue;
+                }
+
+                var existingTag = Database.Session.Query<Tag>().FirstOrDefault(t => t.Name == tag.Name);
+                if (existingTag != null)
+                {
+                    yield return existingTag;
+                    continue;
+                }
+
+                var newTag = new Tag
+                {
+                    Name = tag.Name,
+                    Slug = tag.Name.Slugify()
+                };
+
+                Database.Session.Save(newTag);
+                yield return newTag;
+            }
         }
     }
 }
